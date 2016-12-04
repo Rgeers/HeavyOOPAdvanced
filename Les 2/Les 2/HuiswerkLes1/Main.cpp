e4756589818b7bd2cbb51bb3a61f8f85d458d0fb
Rgeers <rik.geersing@gmail.com>
2016-12-04 21:54:25 +0100

    Huiswerk verbeterd

--- Les 2/Les 2/HuiswerkLes1/Main.cpp
+++ Les 2/Les 2/HuiswerkLes1/Main.cpp
@@ -4,23 +4,27 @@
 #include "Config.h"
 
 int main() {
-
+	string transactionHistory;
+	Config bankAccount = Config("SimpleConfig");
 	BankAccount account = BankAccount(0, "My saldo", 0);
 	Transaction transaction1 = Transaction(true, 9001, "07-03-1993");
+
+
+	
+
 
 	cout << account.getName() << " MONEY = " << account.getSaldo() << endl;
 	account = account + transaction1;
 	cout << account.getName() << " MONEY = " << account.getSaldo() << endl;
 	account.TransactionHistory();
-	
-	Config config = Config("SimpleConfig");
-	config.put("key1", "value1");
-	config.put("key2", "value2");
 
-	cout << config["key1"] << endl;
-	cout << config["key2"] << endl;
-	cout << config["key3"] << endl;
-	cout << config << endl;
+	std::string saldo = std::to_string(account.getSaldo());
+	transactionHistory = "Number of transactions: " + account.getNumberOfTransactions();
+	
+	bankAccount.put("key1", saldo);
+	bankAccount.put("key2", transactionHistory);
+
+	cout << bankAccount << endl;
 	string input;
 	cin >> input;
 
---
+++ Les 3/READMEFIRST.txt
@@ -0,0 +1,5 @@
+Hoi!
+
+Dit is letterlijk jouw code, Edwin,
+
+Ik was te laat in de les, waardoor ik niet heb kunnen meeschrijven. Om de in-de-les-opdracht-locatie niet leeg te hebben, heb ik 'm even opgevuld met jouw code. Scheelt overtypen, zoals in de les. :P
