6c7f56b674c4cc92f6fbd315e1168a665a66a16c
Rgeers <rik.geersing@gmail.com>
2016-12-12 03:30:38 +0100

    !
    !

--- Les 3/Huiswerk Les 3/SortingTool/SortingTool/Main.cpp
+++ Les 3/Huiswerk Les 3/SortingTool/SortingTool/Main.cpp
@@ -3,29 +3,55 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-vector<string> stringArray;
-vector<float> floatArray;
+
+
+
+/*
+void sort(int input[], int count) {
+	int tmp;
+	//bubblesort
+	for (int i = 0; i < count; i++) {
+
+	}
+}
+*/
+
 template<typename T>
-T sortingTool(T first, T second) {
-	
-	std::sort();
-	if (first > second) return first;
-	return second;
+void print(T input[], int count) {
+	for (int i = 0; i < count; i++) {
+		count << input[i] << endl;
+	}
 }
 
-
-
+template<typename T>
+void sort(T input[], int count) {
+	T tmp;
+	//bubblesort
+	for (int i = 0; i < count; i++) {
+		for (int j = i + 1; j < count; j++) {
+			if (input[j] < input[i]) {
+				tmp = input[i];
+				input[i] = input[j];
+				input[j] = tmp;
+			}
+		}
+	}
+}
 
 int main() {
-
-
-	for (int i : {1, 3, 180, 2, -15}) {
+	/*for (int i : {1, 3, 180, 2, -15}) {
 		floatArray.push_back(i);
 	}
-
 	for (string s : {"aap", "noot", "mies", "boom", "roos", "vuur"}) {
-		cout << sortingTool<string>(s, "henkie") << endl;
+		//cout << sortingTool<string>(s, "henkie") << endl;
 	}
+	*/
+	string stringArray[] = { "aap", "noot", "mies", "boom", "roos", "vuur" };
+	float floatArray[] = { 1, 3, 180, 2, -15 };
+	sort(floatArray, 5);
+	sort(stringArray, 5);
+	//print(floatArray, 5);
+
 	string input;
 	cin >> input;
 	return 0;
